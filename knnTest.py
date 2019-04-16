aaddc9a2beb35255be5e51bdf13efce3f03dc123
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-04-16 17:16:13 +0100

    Fiddled around trying to do knn without corss-validation
    Not complete, might just try with actual bijel data instead.

--- knnTest.py
+++ knnTest.py
@@ -38,3 +38,18 @@ knn1_gscv = cv.GridSearchCV(knn1, param_grid, cv=10)
 knn1_gscv.fit(x1.reshape(-1,1),y)
 print(knn1_gscv.best_params_)
 print(knn1_gscv.best_score_)
+
+# trainInd = [18,93,  91,  92, 126, 149, 2,  34,  95,  73,  98,  76,  40, 127, 138, 114,  39,  36,  25, 31,  42, 136,  21,   6,  28, 102,  66, 113, 125, 137,  55,  32, 133,  60,  22,  88,  23,  30, 112,  90,  61,  71, 143,  67,  35,  53, 109,  50, 132,  78,   8, 131, 104,  49,  15,  48,  47,  70,  17, 101, 148,   4, 146, 144, 128, 110,  26,  43,   5,  46,  10, 140,  87,  85,   7, 134,  29, 121,  24, 142,  65,  33,  80,  37,  13,  59, 122,  20,  68, 120,  62,  54, 100,  58, 141,  74, 147, 111, 145,  38]
+#
+# trainInd = trainInd-np.repeat(1,len(trainInd))
+# xArr=np.array(x)
+# xArr[18]
+# xTrain = xArr[trainInd]
+# xTrain[0:5]
+#
+# xTest[0:5]
+# yTrain=y[trainInd]
+#
+# knnplain=nn.KNeighborsClassifier()
+# knnplain.fit(xTrain,yTrain)
+# plainTest = knnplain.predict()
--- python_comp.R
+++ python_comp.R
@@ -4,10 +4,31 @@ dat=iris
 head(dat)
 
 train_control <- trainControl(method="cv", number=10)
-
+set.seed(1234)
 knn_fit <- train(Species ~., data=dat, method="knn", trControl=train_control, tuneLength=34)
 print(knn_fit)
 plot(knn_fit)
 
-
 print(paste0("Accuracy: ",knn_fit$results[row.names(knn_fit$bestTune),]$Accuracy, " (k=", knn_fit$bestTune[[1]], ")"))
+
+
+dat1=data.frame(Sepal.Width=dat$Sepal.Width, Species=dat$Species)
+head(dat1)
+set.seed(1234)
+knn_fit1 <- train(Species ~., data=dat1, method="knn", trControl=train_control, tuneLength=34)
+print(knn_fit1)
+plot(knn_fit1)
+
+print(paste0("Accuracy: ",knn_fit1$results[row.names(knn_fit1$bestTune),]$Accuracy, " (k=", knn_fit1$bestTune[[1]], ")"))
+
+smp_size <- floor(0.67 * nrow(dat))
+set.seed(1234)
+train_ind <- sample(seq_len(nrow(dat)), size=smp_size)
+
+dat_train <- dat[train_ind,]
+head(dat_train)
+dat_test <- dat[-train_ind,]
+
+knn_plain <- train(Species ~., data=dat, method="knn")
+print(paste0("Accuracy: ",knn_plain$results[row.names(knn_plain$bestTune),]$Accuracy, " (k=", knn_plain$bestTune[[1]], ")"))
+
