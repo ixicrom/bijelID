b88be29b5535ceb53a3e0dbfab70ab1316591694
Emily Gould <e.m.gould@sms.ed.ac.uk>
2019-07-17 16:20:34 +0100

    sameSample_analysis.R fixed model variables calculations for new images

--- sameSample_analysis.R
+++ sameSample_analysis.R
@@ -78,45 +78,92 @@ failFilesP <- list.files("/Volumes/PhD/BijelData/sample54i/particle", pattern=".
 autoCorrBL <- do.call(cbind, lapply(bijelFilesL, read.csv, header=FALSE))
 autoCorrBP <- do.call(cbind, lapply(bijelFilesP, read.csv, header=FALSE))
 bijelID <- sapply(strsplit(bijelFileNames,"_"), `[`,1)
+#colnames(autoCorrBL) <- colnames(autoCorrBP) <- bijelID
+
 
 bijelLabs <- rep_len("y", length(bijelFileNames))
 bDat <- data.frame(Bijel=bijelLabs)
-bDat$Liq <- t(autoCorrBL)
-bDat$Par <- t(autoCorrBP)
+rownames(bDat) <- bijelID
+bDat$Liq <- data.frame(t(autoCorrBL))
+bDat$Par <- data.frame(t(autoCorrBP))
 
 
 autoCorrNBL <- do.call(cbind, lapply(failFilesL, read.csv, header=FALSE))
 autoCorrNBP <- do.call(cbind, lapply(failFilesP, read.csv, header=FALSE))
 failID <- sapply(strsplit(failFileNames,"_"), `[`,1)
+#colnames(autoCorrNBL) <- colnames(autoCorrNBP) <- failID
 
 failLabs <- rep_len("n", length(failFileNames))
 nbDat <- data.frame(Bijel=failLabs)
-nbDat$Liq <- t(autoCorrNBL)
-nbDat$Par <- t(autoCorrNBP)
+#rownames(nbDat) <- failID
+nbDat$Liq <- data.frame(t(autoCorrNBL))
+nbDat$Par <- data.frame(t(autoCorrNBP))
 
-testDat <- rbind(bDat, nbDat)
+# testDat <- merge(bDat, nbDat)
+# 
+# y <- testDat$Par[,1:20]
+# lineFits <- lapply(1:19, function(n) lm(unlist(y[n,]) ~ r[1:20]))
+# lineCoeffs <- lapply(lineFits, function(m) m$coefficients)
+# lineGradients <- lapply (1:19, function(p) unname(lineCoeffs[[p]][2]))
+# testDat$Particle.Gradients.20 <- unlist(lineGradients)
+# 
+# y2 <- testDat$Par[,1:10]
+# lineFits <- lapply(1:19, function(n) lm(unlist(y[n,]) ~ r[1:10]))
+# lineCoeffs <- lapply(lineFits, function(m) m$coefficients)
+# lineGradients <- lapply (1:19, function(p) unname(lineCoeffs[[p]][2]))
+# testDat$Particle.Gradients.10 <- unlist(lineGradients)
+# 
+# lTurns <- lapply(1:19, function(y) turnpoints(unlist(testDat$Liq[y,])))
+# firstTurn <- lapply(1:19, function(y) lTurns[[y]]$tppos[1])
+# testDat$Liquid.First.Turn <- unlist(firstTurn)
+# 
+# attach(testDat)
+# testing <- data.frame(Particle.Gradients.10, Particle.Gradients.20, Liquid.First.Turn)
+# 
+# predictA <- predict(finalFit)
+# predictB <- extractPrediction(list(finalFit), testX=testing, testY=testDat$Bijel)
+# 
+# predict(finalFit, newdata = testDat)
 
-y <- testDat$Part[1:20]
-lineFits <- lapply(1:19, function(n) lm(unlist(y[n,]) ~ r[1:20]))
-lineCoeffs <- lapply(lineFits, function(m) m$coefficients)
-lineGradients <- lapply (1:19, function(p) unname(lineCoeffs[[p]][2]))
-testDat$Particle.Gradients.20 <- unlist(lineGradients)
+yB <- bDat$Par[1:20]
+lineFitsB <- lapply(1:8, function(n) lm(unlist(yB[n,]) ~ r[1:20]))
+lineCoeffsB <- lapply(lineFitsB, function(m) m$coefficients)
+lineGradientsB <- lapply(1:8, function(p) unname(lineCoeffsB[[p]][2]))
+bDat$Particle.Gradients.20 <- unlist(lineGradientsB)
 
-y2 <- testDat$Part[1:10]
-lineFits <- lapply(1:19, function(n) lm(unlist(y[n,]) ~ r[1:10]))
-lineCoeffs <- lapply(lineFits, function(m) m$coefficients)
-lineGradients <- lapply (1:19, function(p) unname(lineCoeffs[[p]][2]))
-testDat$Particle.Gradients.10 <- unlist(lineGradients)
+yB2 <- bDat$Par[1:10]
+lineFitsB2 <- lapply(1:8, function(n) lm(unlist(yB2[n,]) ~ r[1:10]))
+lineCoeffsB2 <- lapply(lineFitsB2, function(m) m$coefficients)
+lineGradientsB2 <- lapply(1:8, function(p) unname(lineCoeffsB2[[p]][2]))
+bDat$Particle.Gradients.10 <- unlist(lineGradientsB2)
 
-lTurns <- lapply(1:19, function(y) turnpoints(unlist(testDat$Liq[y,])))
-firstTurn <- lapply(1:19, function(y) liquidTurns[[y]]$tppos[1])
-testDat$Liquid.First.Turn <- unlist(firstTurn)
+lTurnsB <- lapply(1:8, function(n) turnpoints(unlist(bDat$Liq[n,])))
+firstTurnB <- lapply(1:8, function(m) lTurnsB[[m]]$tppos[1])
+bDat$Liquid.First.Turn <- unlist(firstTurnB)
 
-attach(testDat)
-testing <- data.frame(Particle.Gradients.10, Particle.Gradients.20, Liquid.First.Turn)
 
-predictA <- predict(finalFit)
-predictB <- extractPrediction(list(finalFit), testX=testing, testY=testDat$Bijel)
+yNB <- nbDat$Par[1:20]
+lineFitsNB <- lapply(1:11, function(n) lm(unlist(yNB[n,]) ~ r[1:20]))
+lineCoeffsNB <- lapply(lineFitsNB, function(m) m$coefficients)
+lineGradientsNB <- lapply(1:11, function(p) unname(lineCoeffsNB[[p]][2]))
+nbDat$Particle.Gradients.20 <- unlist(lineGradientsNB)
 
+yNB2 <- nbDat$Par[1:10]
+lineFitsNB2 <- lapply(1:11, function(n) lm(unlist(yNB2[n,]) ~ r[1:10]))
+lineCoeffsNB2 <- lapply(lineFitsNB2, function(m) m$coefficients)
+lineGradientsNB2 <- lapply(1:11, function(p) unname(lineCoeffsNB2[[p]][2]))
+nbDat$Particle.Gradients.10 <- unlist(lineGradientsNB2)
+
+lTurnsNB <- lapply(1:11, function(n) turnpoints(unlist(nbDat$Liq[n,])))
+firstTurnNB <- lapply(1:11, function(m) lTurnsNB[[m]]$tppos[1])
+nbDat$Liquid.First.Turn <- unlist(firstTurnNB)
+
+attach(bDat)
+testDat1 <- data.frame(Particle.Gradients.10, Particle.Gradients.20, Liquid.First.Turn)
+
+attach(nbDat)
+testDat2 <- data.frame(Particle.Gradients.10, Particle.Gradients.20, Liquid.First.Turn)
+
+testDat <- rbind(testDat1, testDat2)
 predict(finalFit, newdata = testDat)
 
